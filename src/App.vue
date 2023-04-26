6821e7a542c1c4b2c5e0984af1d3556aa12d9511
Andres Cayambe <andrescayambe99@hotmail.com>
2023-04-26 20:16:31 +0200

    [end]innest-component-apptitle

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+  name: "App",
+  components:{
+    AppTitle
+  },
+  data(){
+    return{
+      title: "ciao"
+    }
+  }
+  
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <AppTitle/>
+</template>
+
+<style>
+
+</style>
